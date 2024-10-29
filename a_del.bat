67459bd0444c2dfd20cd1c2d75924b13f92682eb
wsbgithub <63290360+wsbgithub@users.noreply.github.com>
2024-10-30 04:54:57 +0800

    打卡对话

--- a_del.bat
+++ a_del.bat
@@ -1,9 +1,21 @@
 @echo off
-echo 正在删除所有 HTML 文件...
- for /r %f in (*.html) do del "%f"
+setlocal
 
-echo 正在删除所有 MP3 文件...
- for /r %f in (*.mp3) do del "%f"
+:: 设置目标文件夹路径
+set "target_folder=E:\source\jieke-base"
 
-echo 文件删除完成。
+:: 删除所有 .html 文件
+for /r "%target_folder%" %%f in (*.html) do (
+    del "%%f"
+    echo Deleted: %%f
+)
+
+:: 删除所有 .mp3 文件
+for /r "%target_folder%" %%f in (*.mp3) do (
+    del "%%f"
+    echo Deleted: %%f
+)
+
+echo All .html and .mp3 files have been deleted.
+endlocal
 pause
